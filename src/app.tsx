1c685752dd848f33840c64ff4a9b584553d616b3
nyinyithann <nyinyithann@gmail.com>
2022-03-13 15:07:11 +0800

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+    return (
+        <div className="container">
+            <p className="greeting">Hello, World</p>
+        </div>
+    )
+}
+
+export default App;
