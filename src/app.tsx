da60cd07d0ab9e30e75db851eed7f6c155e5710f
Nyi Nyi <nyinyithann@gmail.com>
2022-08-31 14:23:03 +0800

    added emotion

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import './index.css';
+import { css } from '@emotion/css';
+import * as React from 'react';
 
 function App() {
-    return (
-        <div className="container">
-            <p className="greeting">Hello, World</p>
-        </div>
-    )
+  return (
+    <div
+      className={css({
+        width: '100vw',
+        margin: 'auto',
+      })}
+    >
+      <p
+        className={css({
+          textAlign: 'center',
+          fontSize: '2rem',
+        })}
+      >
+        Hello, World
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 import '../node_modules/modern-normalize/modern-normalize.css';
 import App from './app';
 
+const containerId = 'react-root';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("react-root")
-);
-
-
+const container = document.getElementById(containerId);
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  const div = document.createElement('div');
+  const text = document.createTextNode(
+    `Element wiht id '${containerId}' not found at 'index.tsx'.`
+  );
+  div.appendChild(text);
+  document.body.appendChild(div);
+}
